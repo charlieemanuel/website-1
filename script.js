3f31823eca41500f088572cdd8618be09337e2c8
Charlie Emanuel <150952598+charlieemanuel@users.noreply.github.com>
2025-09-18 15:08:58 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+function showSideBarSm(){
+    const sideBarsm = document.querySelector('.sidebar-small');
+    const menuButton = document.querySelector('.menu-button-small')
+    sideBarsm.style.display = 'block'
+    menuButton.style.display = 'none'
+}
+
+function hideSideBarSm(){
+        const sideBarsm = document.querySelector('.sidebar-small');
+        const menuButton = document.querySelector('.menu-button-small')
+        sideBarsm.style.display = 'none';
+        menuButton.style.display = 'block'
+}
